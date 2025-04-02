6e1f893cb5e4b0598208a19a99c051c90b79327a
lubasha <luba.selina.2003@mail.ru>
2025-04-03 00:14:16 +0300

    Update getcalendar.js

--- netlify/functions/getcalendar.js
+++ netlify/functions/getcalendar.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
     const duration = event.queryStringParameters.duration;
-    const url = `https://arabia-refurbished-palm-accepting.trycloudflare.com?duration=${duration}`;
+    const url = `https://lab-review-nurses-shape.trycloudflare.com/api/calendar?duration=${duration}`;
     
     try {
         const response = await fetch(url);
