0c0e81a754f2e6a8cbc80dd3af2da0784dcc89f8
lubasha <luba.selina.2003@mail.ru>
2025-04-30 20:04:44 +0300

    Update getcalendar.js

--- netlify/functions/getcalendar.js
+++ netlify/functions/getcalendar.js
@@ -22,7 +22,7 @@ exports.handler = async (event) => {
       };
     }
 
-    const apiUrl = `https://moses-checkout-wells-choosing.trycloudflare.com/api/calendar?id_service=${id_service}`;
+    const apiUrl = `https://buildings-cb-visited-thorough.trycloudflare.com/api/calendar?id_service=${id_service}`;
     console.log('Requesting:', apiUrl);
     
     const response = await fetch(apiUrl);
