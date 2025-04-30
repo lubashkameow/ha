bd3f7d594b820ab557c42148b6f0ecf08ce024e5
lubasha <luba.selina.2003@mail.ru>
2025-04-30 21:15:01 +0300

    Update gettimeslots.js

--- netlify/functions/gettimeslots.js
+++ netlify/functions/gettimeslots.js
@@ -1,8 +1,16 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
-    const { date, duration } = event.queryStringParameters;
-    const url = `https://buildings-cb-visited-thorough.trycloudflare.com/api/timeslots?date=${date}&duration=${duration}`;
+    const { date, master_id, duration } = event.queryStringParameters;
+    
+    if (!date || !master_id) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: 'date and master_id parameters are required' })
+        };
+    }
+    
+    const url = `https://buildings-cb-visited-thorough.trycloudflare.com/api/timeslots?date=${date}&master_id=${master_id}&duration=${duration || 60}`;
     
     try {
         const response = await fetch(url);
