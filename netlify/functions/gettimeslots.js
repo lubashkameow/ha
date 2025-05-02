34a625218618ce2c8aff2745ac6eceeb3d4e4032
lubasha <luba.selina.2003@mail.ru>
2025-05-02 20:32:07 +0300

    Update gettimeslots.js

--- netlify/functions/gettimeslots.js
+++ netlify/functions/gettimeslots.js
@@ -10,7 +10,7 @@ exports.handler = async (event) => {
         };
     }
     
-    const url = `https://powers-mug-highlights-populations.trycloudflare.com/api/timeslots?date=${date}&master_id=${master_id}&duration=${duration || 60}`;
+    const url = `https://plastic-reproduction-resorts-barbara.trycloudflare.com/api/timeslots?date=${date}&master_id=${master_id}&duration=${duration || 60}`;
     
     try {
         const response = await fetch(url);
