289b55c862bae5f6a233698e8b43b7ef417582a8
lubasha <luba.selina.2003@mail.ru>
2025-05-02 20:34:11 +0300

    Update getcalendar.js

--- netlify/functions/getcalendar.js
+++ netlify/functions/getcalendar.js
@@ -22,7 +22,7 @@ exports.handler = async (event) => {
       };
     }
 
-    const apiUrl = `https://powers-mug-highlights-populations.trycloudflare.com/api/calendar?id_service=${id_service}`;
+    const apiUrl = `https://plastic-reproduction-resorts-barbara.trycloudflare.com/api/calendar?id_service=${id_service}`;
     console.log('Requesting:', apiUrl);
     
     const response = await fetch(apiUrl);
