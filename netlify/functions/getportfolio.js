1e62089ca0f22a3b5dae6cbb723588f7e3c62617
lubasha <luba.selina.2003@mail.ru>
2025-05-02 20:32:54 +0300

    Update getportfolio.js

--- netlify/functions/getportfolio.js
+++ netlify/functions/getportfolio.js
@@ -11,7 +11,7 @@ exports.handler = async (event) => {
     }
 
     const response = await fetch(
-      `https://powers-mug-highlights-populations.trycloudflare.com/api/portfolio?master_id=${masterId}`
+      `https://plastic-reproduction-resorts-barbara.trycloudflare.com/api/portfolio?master_id=${masterId}`
     );
 
     if (!response.ok) throw new Error('API request failed');
