6dc04b9916181e45d0e6d989b728a4516c0caed3
lubasha <luba.selina.2003@mail.ru>
2025-05-04 10:47:52 +0300

    Update getappointments.js

--- netlify/functions/getappointments.js
+++ netlify/functions/getappointments.js
@@ -11,7 +11,7 @@ exports.handler = async (event) => {
     }
 
     const response = await fetch(
-      `https://plastic-reproduction-resorts-barbara.trycloudflare.com/api/appointments?user_id=${userId}`
+      `https://equity-connectivity-loops-entertainment.trycloudflare.com/api/appointments?user_id=${userId}`
     );
 
     if (!response.ok) throw new Error('API request failed');
