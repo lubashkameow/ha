62818d48178084e98e223ae804d02e3fead07ebc
lubasha <luba.selina.2003@mail.ru>
2025-05-04 12:03:20 +0300

    Create getmasters.js

---
+++ netlify/functions/getmasters.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+
+exports.handler = async () => {
+  try {
+    const response = await fetch('https://equity-connectivity-loops-entertainment.trycloudflare.com/api/masters');
+    if (!response.ok) throw new Error('API request failed');
+    
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
