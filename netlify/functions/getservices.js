c6eaea6d05ebd91604e2837ad7ba83cbe9603d2f
lubasha <luba.selina.2003@mail.ru>
2025-05-05 13:27:09 +0300

    Update getservices.js

--- netlify/functions/getservices.js
+++ netlify/functions/getservices.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 exports.handler = async () => {
   try {
-    const response = await fetch('https://equity-connectivity-loops-entertainment.trycloudflare.com/api/services');
+    const response = await fetch('https://same-impacts-theories-larger.trycloudflare.com/api/services');
     
     if (!response.ok) throw new Error('API request failed');
     
