af0617dbb51ba307b462b73f35d6fd32eef634f9
lubasha <luba.selina.2003@mail.ru>
2025-05-19 23:27:17 +0300

    Update getappointments.js

--- netlify/functions/getappointments.js
+++ netlify/functions/getappointments.js
@@ -11,7 +11,7 @@ exports.handler = async (event) => {
     }
 
     const response = await fetch(
-      `https://same-impacts-theories-larger.trycloudflare.com/api/appointments?user_id=${userId}`
+      `https://want-nursing-period-noise.trycloudflare.com/api/appointments?user_id=${userId}`
     );
 
     if (!response.ok) throw new Error('API request failed');
