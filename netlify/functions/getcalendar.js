ba48d298f22e9eb31088fb2ba6456d4eabfb9e00
lubasha <luba.selina.2003@mail.ru>
2025-05-20 23:23:24 +0300

    Update getcalendar.js

--- netlify/functions/getcalendar.js
+++ netlify/functions/getcalendar.js
@@ -22,7 +22,7 @@ exports.handler = async (event) => {
       };
     }
 
-    const apiUrl = `https://want-nursing-period-noise.trycloudflare.com/api/calendar?id_service=${id_service}`;
+    const apiUrl = `https://concept-bedrooms-christopher-guide.trycloudflare.com/api/calendar?id_service=${id_service}`;
     console.log('Requesting:', apiUrl);
     
     const response = await fetch(apiUrl);
