0d9786871616eb4e75fb69671258d150e694941b
lubasha <luba.selina.2003@mail.ru>
2025-05-22 17:11:42 +0300

    Create getapp.js

---
+++ netlify/functions/getapp.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+    const { user_id, date } = event.queryStringParameters;
+    const apiUrl = `https://monkey-bandwidth-suggesting-murray.trycloudflare.com/api/app?user_id=${user_id}&date=${date}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        };
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: e.message })
+        };
+    }
+};
