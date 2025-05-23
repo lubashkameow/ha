11b0c49c68472348c0a0d657fa83f2d718b4b292
lubasha <luba.selina.2003@mail.ru>
2025-05-23 12:56:35 +0300

    Update getportfolio.js

--- netlify/functions/getportfolio.js
+++ netlify/functions/getportfolio.js
@@ -11,7 +11,7 @@ exports.handler = async (event) => {
     }
 
     const response = await fetch(
-      `https://monkey-bandwidth-suggesting-murray.trycloudflare.com/api/portfolio?master_id=${masterId}`
+      `https://psi-wires-catch-italia.trycloudflare.com/api/portfolio?master_id=${masterId}`
     );
 
     if (!response.ok) throw new Error('API request failed');
