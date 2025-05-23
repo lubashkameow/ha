6f31edf89aa1fb4004d0a2ea665a18bcbf93f34c
lubasha <luba.selina.2003@mail.ru>
2025-05-23 14:29:49 +0300

    Create toggleworkday.js

---
+++ netlify/functions/toggleworkday.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+    const apiUrl = `https://psi-wires-catch-italia.trycloudflare.com/api/toggleworkday`;
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: event.body
+        });
+        const data = await response.json();
+        return {
+            statusCode: response.ok ? 200 : response.status,
+            headers: { 'Access-Control-Allow-Origin': '*' },
+            body: JSON.stringify(data)
+        };
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: e.message })
+        };
+    }
+};
