bb910cfa2f488f88a4f1e52f13b1ad9dc8694ef1
lubasha <luba.selina.2003@mail.ru>
2025-05-23 14:29:24 +0300

    Create getworkday.js

---
+++ netlify/functions/getworkday.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+    const { user_id, date } = event.queryStringParameters;
+    const apiUrl = `https://psi-wires-catch-italia.trycloudflare.com/api/getworkday?user_id=${user_id}&date=${date}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return {
+            statusCode: response.ok ? 200 : response.status,
+            headers: { 'Access-Control-Allow-Origin': '*' },
+            body: JSON.stringify(data)
+        };
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: e.message })
+        };
+    }
+};
