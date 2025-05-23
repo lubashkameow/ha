ee25a5b87fcc652168ecca2011cebe4a2692e3e3
lubasha <luba.selina.2003@mail.ru>
2025-05-23 14:29:06 +0300

    Create getcalendardata.js

---
+++ netlify/functions/getcalendardata.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+    const { user_id, month } = event.queryStringParameters;
+    const apiUrl = `https://psi-wires-catch-italia.trycloudflare.com/api/getcalendardata?user_id=${user_id}&month=${month}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return {
+            statusCode: response.ok ? 200 : response.status,
+            headers: { 'Access-Control-Allow-Origin': '*' },
+            body: JSON.stringify(data)
+        };
+    } catch (e) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: e.message })
+        };
+    }
+};
