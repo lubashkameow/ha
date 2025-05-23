67b3eaaa2d0d7dd503a2d5b56b91c55873d4962b
lubasha <luba.selina.2003@mail.ru>
2025-05-23 16:00:36 +0300

    Update gettimeslots.js

--- netlify/functions/gettimeslots.js
+++ netlify/functions/gettimeslots.js
@@ -10,7 +10,7 @@ exports.handler = async (event) => {
         };
     }
     
-    const url = `https://psi-wires-catch-italia.trycloudflare.com/api/timeslots?date=${date}&master_id=${master_id}&duration=${duration || 60}`;
+    const url = `https://probability-published-oxide-warcraft.trycloudflare.com/api/timeslots?date=${date}&master_id=${master_id}&duration=${duration || 60}`;
     
     try {
         const response = await fetch(url);
