dcae23b8d60e97ab45fd4722c0c32aa82f38ef2a
lubasha <luba.selina.2003@mail.ru>
2025-05-24 14:54:32 +0300

    Update update_portfolio_description.js

--- netlify/functions/update_portfolio_description.js
+++ netlify/functions/update_portfolio_description.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
-  const apiUrl = 'https://probability-published-oxide-warcraft.trycloudflare.com/api/update_portfolio_description';
+  const apiUrl = 'https://female-bias-wrap-merchandise.trycloudflare.com/api/update_portfolio_description';
 
   try {
     const { user_id, photo_id, description } = JSON.parse(event.body);
