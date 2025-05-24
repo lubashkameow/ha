99f7dfeb97885fb28e0898e53d78383c674e5124
lubasha <luba.selina.2003@mail.ru>
2025-05-24 23:49:33 +0300

    Create isadmin.js

---
+++ netlify/functions/isadmin.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+  try {
+    const response = await fetch(`https://revised-bios-collection-gather.trycloudflare.com/api/isadmin?${new URLSearchParams(event.queryStringParameters)}`);
+    if (!response.ok) throw new Error('API request failed');
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
