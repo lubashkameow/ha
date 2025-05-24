c4f0ba524c074e67d372a21cea1d7fbb8c50c1fd
lubasha <luba.selina.2003@mail.ru>
2025-05-24 23:50:30 +0300

    Create deleteservice.js

---
+++ netlify/functions/deleteservice.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+  try {
+    const response = await fetch('https://revised-bios-collection-gather.trycloudflare.com/api/deleteservice', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: event.body
+    });
+    if (!response.ok) throw new Error('API request failed');
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
