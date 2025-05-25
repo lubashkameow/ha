94f2104f3ae72d231c4860fd9e29e51c1a42d08c
lubasha <luba.selina.2003@mail.ru>
2025-05-25 16:10:35 +0300

    Update toggleworkday.js

--- netlify/functions/toggleworkday.js
+++ netlify/functions/toggleworkday.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
-    const apiUrl = `https://revised-bios-collection-gather.trycloudflare.com/api/toggleworkday`;
+    const apiUrl = `https://biotechnology-les-nu-cleaning.trycloudflare.com/api/toggleworkday`;
 
     try {
         const response = await fetch(apiUrl, {
