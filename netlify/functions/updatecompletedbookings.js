491a40b95cf5e9c80ce678a4739a627674b05c07
lubasha <luba.selina.2003@mail.ru>
2025-05-25 16:09:41 +0300

    Update updatecompletedbookings.js

--- netlify/functions/updatecompletedbookings.js
+++ netlify/functions/updatecompletedbookings.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 exports.handler = async () => {
     try {
         const response = await fetch(
-            'https://revised-bios-collection-gather.trycloudflare.com/api/update_completed_bookings',
+            'https://biotechnology-les-nu-cleaning.trycloudflare.com/api/update_completed_bookings',
             {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' }
