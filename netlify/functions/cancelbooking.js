42ef7c6a87f2b0db997c394b8f8744ffc48c874f
lubasha <luba.selina.2003@mail.ru>
2025-05-28 11:38:56 +0300

    Update cancelbooking.js

--- netlify/functions/cancelbooking.js
+++ netlify/functions/cancelbooking.js
@@ -11,7 +11,7 @@ exports.handler = async (event) => {
       };
     }
 
-    const response = await fetch('https://biotechnology-les-nu-cleaning.trycloudflare.com/api/bookings/cancel', {
+    const response = await fetch('https://leon-dec-becomes-result.trycloudflare.com/api/bookings/cancel', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ booking_id }),
