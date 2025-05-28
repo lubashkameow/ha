1db8fcc6ece47bb6dd53abd1d3cd20f27f06dbd6
lubasha <luba.selina.2003@mail.ru>
2025-05-28 11:38:41 +0300

    Update createbooking.js

--- netlify/functions/createbooking.js
+++ netlify/functions/createbooking.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
     const body = JSON.parse(event.body);
-    const url = `https://biotechnology-les-nu-cleaning.trycloudflare.com/api/bookings`;
+    const url = `https://leon-dec-becomes-result.trycloudflare.com/api/bookings`;
     
     try {
         const response = await fetch(url, {
