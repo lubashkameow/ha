dbb7a196af32fb00144124a6eec031c60b65178e
lubasha <luba.selina.2003@mail.ru>
2025-05-28 11:35:37 +0300

    Update getportfolio.js

--- netlify/functions/getportfolio.js
+++ netlify/functions/getportfolio.js
@@ -11,7 +11,7 @@ exports.handler = async (event) => {
     }
 
     const response = await fetch(
-      `https://biotechnology-les-nu-cleaning.trycloudflare.com/api/portfolio?master_id=${masterId}`
+      `https://leon-dec-becomes-result.trycloudflare.com/api/portfolio?master_id=${masterId}`
     );
 
     if (!response.ok) throw new Error('API request failed');
