b5a4065268c8f8c71177598a5bebb73b45c629cd
lubasha <luba.selina.2003@mail.ru>
2025-06-22 19:31:14 +0300

    Update updateservice.js

--- netlify/functions/updateservice.js
+++ netlify/functions/updateservice.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 exports.handler = async (event) => {
   try {
-    const response = await fetch('https://friday-newton-emerging-dh.trycloudflare.com/api/updateservice', {
+    const response = await fetch('https://cad-specifies-slovakia-proof.trycloudflare.com/api/updateservice', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: event.body
